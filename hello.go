e7e2555c87446e2c29102a285ebe301c45809580
Dave Robinson <davidtrobinson@gmail.com>
2018-08-13 07:12:16 -0600

    Added call to reverse string

--- hello.go
+++ hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	 "fmt"
+
+	 "github.com/davidtrobinson/stringutil"
+)
 
 func main() {
-	fmt.Printf("Hello, world. You rock!\n")
+	fmt.Printf(stringutil.Reverse("\n!oG ,olleH"))
 }
